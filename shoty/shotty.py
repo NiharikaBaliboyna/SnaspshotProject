86a36679280321c9b794c0e43c342148f2e3342e
Baliboyna <balibon@a483e763089c.ant.amazon.com>
2020-02-17 14:01:43 +0530

    shoty v2

--- shoty/shotty.py
+++ shoty/shotty.py
@@ -1,8 +1,12 @@
 import boto3
 
 
-if __name__=='__main__':
-    session=boto3.Session(profile_name='shoty')
-    ec2=session.resource('ec2')
+session=boto3.Session(profile_name='shoty')
+ec2=session.resource('ec2')
+
+def list_instances():
     for i in ec2.instances.all():
-        print(i)
+        print(','.join((i.id,i.instance_type,i.placement['AvailabilityZone'],i.state['Name'],i.public_dns_name)))
+    return
+if __name__=='__main__':
+    list_instances()
